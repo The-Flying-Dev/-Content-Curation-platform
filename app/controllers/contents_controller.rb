dbbc6c9a016591ce0c98016d6350389081be5d89
Leandran <leandranm@outlook.com>
2021-10-22 19:30:48 +0200

    added actions to comments/contents controller

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -5,6 +5,50 @@ class ContentsController < ApplicationController
     end
 
     def show
-        
+        content = Content.find(params[:id])
+        if content
+            render json: {
+                title: content.title,
+                url: content.url,
+                image: content.image,
+                description: content.description,
+                user: content.user,
+                comment: content.comment
+            }
+
+        else 
+            render json: {message: "Can't find the content"}
+        end
     end
+
+    def create 
+        content = Content.new
+        content.title = params[:title]
+        content.url = params[:url]
+        content.description = params[:description]
+        content.image = params[:image]
+        content.save
+        if Content.save 
+            render json: content
+        else
+            render json: {message: "Error creating a content"}
+        end
+    end
+
+    def destroy
+        content = Content.find(params[:id])
+        if content
+            content.destroy
+            render json: {message: "Deleted successfully"}
+        else 
+            render json: {message: "Cant find this content"}
+        end
+    end
+
+    private
+
+    def content_params
+        params.permit(:title, :url, :image, :description)
+    end
+    
 end
